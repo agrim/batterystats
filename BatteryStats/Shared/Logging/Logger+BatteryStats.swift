c6958917b8bbda7a8d3ddc68076370825dcc6a8f
Agrim <129019+agrim@users.noreply.github.com>
2026-07-03 10:50:40 +0400

    Remove unused logger categories

--- BatteryStats/Shared/Logging/Logger+BatteryStats.swift
+++ BatteryStats/Shared/Logging/Logger+BatteryStats.swift
@@ -6,6 +6,4 @@ extension Logger {
 
     static let batteryReader = Logger(subsystem: subsystem, category: "batteryReader")
     static let powerSource = Logger(subsystem: subsystem, category: "powerSource")
-    static let settingsSync = Logger(subsystem: subsystem, category: "settingsSync")
-    static let launchAtLogin = Logger(subsystem: subsystem, category: "launchAtLogin")
 }
